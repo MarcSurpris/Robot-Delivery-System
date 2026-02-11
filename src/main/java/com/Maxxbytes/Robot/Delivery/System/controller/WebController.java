1f02d1243e8477c5e791a6470706f6ac7bd7e228
Autumn Willis <autumnm3841@gmail.com>
2026-02-11 09:26:42 -0500

    Added server-rendered Orders view and integrated it with the service layer

--- src/main/java/com/Maxxbytes/Robot/Delivery/System/controller/WebController.java
+++ src/main/java/com/Maxxbytes/Robot/Delivery/System/controller/WebController.java
@@ -4,6 +4,8 @@ import com.Maxxbytes.Robot.Delivery.System.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -19,5 +21,16 @@ public class WebController {
     public String index(Model model) {
         model.addAttribute("orders", orderService.getAllOrders());
         return "index";
+    }
+
+    @PostMapping("/orders/create")
+    public String createOrder(@RequestParam String address) {
+
+
+        orderService.createOrder(1L, address);
+
+        return "redirect:/";
+
+
     }
 }
